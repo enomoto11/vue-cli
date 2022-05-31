8d56fd7f55f8ec39d491c90cb0a5d6a1eb17f7fe
enomoto-catallaxy <t.enomoto@catallaxy.me>
2022-05-31 20:31:02 +0900

    メモアプリ修正途中

--- src/components/MemoIndex.vue
+++ src/components/MemoIndex.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="memo">
     <h1>Input your new note!</h1>
+    <button @click="allDelete()"> localstrage削除</button>
     <ul>
-      <li v-for="(item, index) in topTodos" :key="index">
-        <span @click="tryToEditTodo(index),isTrueToEdit()">
-          {{ item }}
+      <li v-for="memo in topTodos" :key="memo.id">
+        <span @click="tryToEditTodo(memo.id), isTrueToEdit()">
+          {{ memo.text }} : {{ memo.id }}
         </span>
       </li>
       <li>
@@ -12,11 +13,11 @@
       </li>
     </ul>
     <div v-if="whetherToAdd">
-      <textarea v-model="addItem"></textarea>
+      <textarea v-model="addText"></textarea>
       <button @click="addTodo()">追加</button><br>
     </div>
     <div v-if="whetherToEdit">
-      <textarea v-model="editItem"></textarea>
+      <textarea v-model="editText"></textarea>
       <button @click="editTodo()">編集</button>
       <button @click="deleteTodo()">削除</button>
     </div>
@@ -32,9 +33,10 @@ export default {
       whetherToEdit: false,
       todos: [],
       topTodos: [],
-      addItem: '',
-      editItem: '',
-      editIndex: '',
+      addText: '',
+      editId: '',
+      editText: '',
+      deleteMemo: ''
     }
   },
   mounted() {
@@ -42,26 +44,37 @@ export default {
       this.todos = JSON.parse(localStorage.getItem('memos'))
     }
     for (let i = 0; i < this.todos.length; i++) {
-      this.topTodos[i] = this.todos[i].split('\n')[0]
+      this.topTodos.push({ text:this.todos[i].text.split('\n')[0], id:this.todos[i].id})
     }
+    console.log(this.todos)
+    console.log(this.topTodos)
   },
   methods: {
-    isTrueToAdd() {
+    getMaxId () {
+      if (this.todos.length !== 0) {
+        const ids = this.todos.map(item => item.id)
+        return Math.max(...ids)
+      } else {
+        return 0
+      }
+    },
+    isTrueToAdd () {
       this.whetherToAdd = true
       this.whetherToEdit = false
       this.editItem = ''
       this.editIndex = ''
     },
-    isTrueToEdit() {
+    isTrueToEdit () {
       this.whetherToAdd = false
       this.whetherToEdit = true
-      this.addItem = ''
+      this.addText = ''
     },
-    addTodo() {
-      if (this.addItem !== '') {
-        this.todos.push(this.addItem)
-        this.topTodos.push(this.addItem.split('\n')[0])
-        this.addItem = ''
+    addTodo () {
+      if (this.addText !== '') {
+        const newId = this.getMaxId() + 1
+        this.todos.push({ text: this.addText, id: newId })
+        this.topTodos.push({ text: this.addText.split('\n')[0], id: newId })
+        this.addText = ''
         this.saveTodo()
       } else {
         window.alert('空欄でTODO LISTへ追加はできません')
@@ -72,26 +85,41 @@ export default {
       localStorage.setItem('memos', parsed);
     },
     deleteTodo() {
-      this.todos.splice(this.editIndex,1)
-      this.topTodos.splice(this.editIndex,1)
-      this.editItem = ''
-      this.editIndex = ''
+      const index = this.todos.findIndex((memo) => memo.id === this.deleteMemo.id)
+      this.todos.splice(index,1)
+      this.topTodos.splice(index,1)
+      this.editText = ''
+      this.deleteMemo = ''
+      this.whetherToEdit = false
       this.saveTodo()
     },
-    tryToEditTodo (index) {
-      this.editItem = this.todos[index]
-      this.editIndex = index
+    tryToEditTodo (id) {
+      const memo = this.todos.find((memo) => memo.id === id)
+      this.editText = memo.text
+      this.editId = id
+      this.deleteMemo = {
+        text: this.editText,
+        id: this.editId
+      }
     },
     editTodo() {
       if (this.editItem !== ''){
-        this.todos.splice(this.editIndex, 1, this.editItem)
-        this.topTodos.splice(this.editIndex, 1, this.editItem.split('\n')[0])
-        this.editItem = ''
-        this.editIndex = ''
+        const todo = this.todos.find((memo) => memo.id === this.deleteMemo.id)
+        todo.text = this.editText
+        const topTodo = this.topTodos.find((memo) => memo.id === this.deleteMemo.id)
+        topTodo.text = this.editText.split('\n')[0]
+        this.editText = ''
+        this.deleteMemo = ''
+        this.whetherToEdit = false
         this.saveTodo()
       } else {
         window.alert('空欄で編集はできません')
       }
+    },
+    allDelete() {
+      this.todos = []
+      this.topTodos = []
+      this.saveTodo()
     }
   }
 }
